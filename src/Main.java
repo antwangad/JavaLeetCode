4d874fe0d24215cd1bb03fc9e51c2590ed29fc83
antwangad <antwangad@cmail.carleton.ca>
2025-06-21 14:03:24 -0400

    Longest Substring without repeats! (medium)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.HashSet;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("LeetCode time!");
+    }
+
+    public int LongestSubstringNoRepeats(String s) {
+        int left = 0;
+        HashSet<Character> set = new HashSet<>();
+        int maxlen = 0;
+
+        for(int right = 0; right< s.length(); right++) {
+            while(set.contains(s.charAt(right))){
+                set.remove(s.charAt(left));
+                left = left + 1;
+            }
+            set.add(s.charAt(right));
+            maxlen = maxlen + 1;
+            maxlen = Math.max(maxlen, right - left + 1);
+        }
+
+        return maxlen;
     }
 }
